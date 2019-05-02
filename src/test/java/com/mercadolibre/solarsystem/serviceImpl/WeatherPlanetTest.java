ad63731e7deede115b1345c7a2786f2e144523ab
MarcosReaper <marcosreaper@gmail.com>
2019-05-01 23:45:58 -0300

    Update WeatherPlanetTest.java

--- src/test/java/com/mercadolibre/solarsystem/serviceImpl/WeatherPlanetTest.java
+++ src/test/java/com/mercadolibre/solarsystem/serviceImpl/WeatherPlanetTest.java
@@ -63,11 +63,6 @@ public class WeatherPlanetTest {
 		Assert.assertTrue(weatherPlanet.equals(weatherPlanetList.get(0)));
 	}
 	
-	@Test
-	public void howManyPeriodsOfDrought() {
-		weatherPlanetService.countByWeatherId(id);
-	}
-	
 	private List<WeatherPlanet> generateWeather(Integer day) {
 		List<WeatherPlanet> weatherPlanetList = new ArrayList<>();
 		
